01f009984f8d4908d2dc5ac5dfae7e86eb306ca4
Alex-SK <aleksey.kozyura@gmail.com>
2020-04-17 22:55:08 +1000

    Add overriding properties & methods

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php
 
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    
     echo 'classes:' . '<br>';
 
     class User {
         
         public $username;
         private $email;
+        public $role = 'member';
 
         public function __construct($username, $email) {
             $this->username = $username;
@@ -14,6 +19,10 @@
 
         public function addFriend() {
             return "$this->username added a new friend";
+        }
+
+        public function message(){
+            return "$this->email sent a new message";
         }
 
         // getters
@@ -34,21 +43,34 @@
     }
 
     class AdminUser extends User {
+        
         public $level;
+        public $role = 'admin';
 
         public function __construct($username, $email, $level) {
             $this->level = $level;
             parent::__construct($username, $email);
         }
+        
+        public function message(){
+            return "$this->email, an admin, sent a new message";
+        }
+
     }
 
     $userOne = new User('mario', 'mario@aaa.com');
     $userTwo = new User('luigi', 'luigi@aaa.com');
     $userThree = new AdminUser('yoshi', 'yoshi@aaa.com', 5);
 
-    echo $userThree->username . '<br>';
-    echo $userThree->getEmail() . '<br>';
-    echo $userThree->level . '<br>';
+    // echo $userThree->username . '<br>';
+    // echo $userThree->getEmail() . '<br>';
+    // echo $userThree->level . '<br>';
+
+    echo $userOne->role . '<br>';
+    echo $userThree->role . '<br>';
+
+    echo $userOne->message() . '<br>';
+    echo $userThree->message() . '<br>';
 
 ?>
 
