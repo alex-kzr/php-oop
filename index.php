84b73fa8f32aec0a27e434bf6ab063ce2d903e2b
Alex-SK <aleksey.kozyura@gmail.com>
2020-04-18 00:36:41 +1000

    Validation. Add validation class

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
 
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    require('user_validator.php');
+
     if(isset($_POST['submit'])){
         // validate entries
-        echo 'form submited <br>';
+        $validation = new UserValidator($_POST);
+        $errors = $validation->validateForm();
+
+        // if errors is empty --> save data to db
     }
 
 ?>
@@ -18,11 +27,18 @@
     <div class="new-user">
         <h2>Create new user</h2>
         <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+            
             <label>Username:</label>
-            <input type="text" name="username">
+            <input type="text" name="username" value="<?php echo htmlspecialchars($_POST['username']) ?? '' ?>">
+            <div class="error">
+                <?php echo $errors['username'] ?? '' ?>
+            </div>
 
             <label>Email:</label>
-            <input type="text" name="email">
+            <input type="text" name="email" value="<?php echo htmlspecialchars($_POST['email']) ?? '' ?>">
+            <div class="error">
+                <?php echo $errors['email'] ?? '' ?>
+            </div>
 
             <input type="submit" value="submit" name="submit">
         </form>
