3e505b27cb6dba7a975a99bfe7e5c3d4da6e4f88
Alex-SK <aleksey.kozyura@gmail.com>
2020-04-17 17:49:56 +1000

    Add constructor

--- index.php
+++ index.php
@@ -4,8 +4,13 @@
 
     class User {
         
-        public $username = 'ryu';
-        public $email = 'ryu@aaa.com';
+        public $username;
+        public $email;
+
+        public function __construct($username, $email) {
+            $this->username = $username;
+            $this->email = $email;
+        }
 
         public function addFriend() {
             return "$this->username added a new friend";
@@ -13,22 +18,16 @@
 
     }
 
-    $userOne = new User();
-    $userTwo = new User();
+    $userOne = new User('mario', 'mario@aaa.com');
+    $userTwo = new User('luigi', 'luigi@aaa.com');
 
     echo $userOne->username . '<br>';
     echo $userOne->email . '<br>';
     echo $userOne->addFriend() . '<br>';
 
-    $userTwo->username = 'yoshi';
-    $userTwo->email = 'yoshi@aaa.com';
-
     echo $userTwo->username . '<br>';
     echo $userTwo->email . '<br>';
     echo $userTwo->addFriend() . '<br>';
-
-    print_r(get_class_vars('User'));
-    print_r(get_class_methods('User'));
 
 ?>
 
